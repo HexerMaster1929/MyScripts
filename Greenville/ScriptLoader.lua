01dd7a50e937445ef5dff928f3fc00a9e4b3ca62
Mark <94270731+HexerMaster1929@users.noreply.github.com>
2022-07-15 19:13:43 -0400

    Update ScriptLoader.lua

--- Greenville/ScriptLoader.lua
+++ Greenville/ScriptLoader.lua
@@ -1,10 +1,14 @@
+wait(1)
+
+if game.PlaceID == 891852901 then
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HexerMaster1929/HexHub/main/UI/Notification%20(Public%20For%20Exploit%20Makers).lua"))()
 
 wait(1)
 
 HexNotification("Greenville Script","Autowash,Autofuel,SpeedMod Loading. Please wait!","rbxassetid://2869102480","5515669992")
 
-wait(1.8)
+wait(0.9)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HexerMaster1929/MyScripts/main/Greenville/GVAUTOREFUEL.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HexerMaster1929/MyScripts/main/Greenville/GVAUTOWASH.lua"))()
@@ -12,3 +16,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/HexerMaster1929/MyScr
 
 HexNotification("Greenville Script","Autowash,Autofuel,SpeedMod Loaded, Enjoy!","rbxassetid://4914902918","9067086414")
 
+end
